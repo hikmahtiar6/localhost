4449787ad97b30b1cab7c81371f832e43e1bde6c
hikmahtiar.cool <hikmahtiar.cool@gmail.com>
2016-08-08 17:27:15 +0700

    feature: pencarian folder

--- file.php
+++ file.php
@@ -1,5 +1,15 @@
 <?php
 require "config.php";
+
+	$search = isset($_POST['search']);
+	$data = glob('*');
+	$text = "";
+
+	if($search) {
+		$text = $_POST['search'];
+	}
+
+	$dataFix = preg_grep("~".preg_quote($text, "~")."~", $data);
 ?>
 
 <!DOCTYPE html>
@@ -43,72 +53,62 @@ require "config.php";
 
 		<div class="row">
 			<div class="col-md-12">
-				<a href="phpmyadmin" target="_blank">
-					<img src="<?php echo $dir; ?>/bg/phpmyadmin.png" width="100">
-				</a>
-				<br>
-				<br>
+				
+				<div class="col-md-6">
+					<a href="phpmyadmin" target="_blank">
+						<img src="<?php echo $dir; ?>/bg/phpmyadmin.png" width="100">
+					</a>
+				</div>
+				<div class="col-md-6">
+					<form method="POST" action="">
+						<input class="col-md-9" name="search" type="text" value="<?php echo $text; ?>" placeholder="masukan kata yang akan dicari.." autocomplete="off"/>
+						<input class="pull-right col-md-3 btn btn-primary" type="submit" value="Cari" />
+					</form>
+				</div>
 			</div>
 		</div>		
 
+		<br>
 		<div class="row">
 			<div style="width: 90%;background: rgba(255,255,255,0.3); padding: 20px 20px 0px 20px;margin: 0px auto !important;">
-			<?php foreach (glob('*') as $folder) : ?>
+			
+			<?php foreach ($dataFix as $folder) : ?>
 				<?php if ($folder != 'index.php' && $folder != 'localhost' && $folder != 'bg' && $folder != 'bower_components' && $folder != 'phpmyadmin') : ?>
-				<?php 
-				$staFolder = stat($folder);
-				$style = '';
-				$icon = 'glyphicon-folder-open';
-				if(!is_dir($folder)) {
-					$style = 'style="color: red;"';
-					$icon = 'glyphicon-file';
-				}
-				?>
-				
-				<div class="col-md-4 folder">
-					<a class="font-folder" href="<?php echo $folder; ?>" target="_blank" <?php echo $style; ?>>
-						<div class="col-md-3">
-							<i class="glyphicon <?php echo $icon; ?>" style="font-size: 50px;"></i>
-						</div>
-						
-						<div class="col-md-6">
-							<b style="font-size: 14px;">
-								<?php echo $folder; ?>
-							</b>
-							<br />
-							<span style="font-size: 12px;">
-								<?php echo date('d M Y H:i:s',$staFolder['mtime']); ?>
-							</span>
-						</div>
-					</a>
-				</div>
-				
-				<!--
-				<div class="col-md-4 folder">
-					<a class="font-folder" href="<?php echo $folder; ?>" target="_blank">
-						<?php if (!is_dir($folder)) : ?>
-						<?php $style = 'style="color: red;"'; ?>
-							<i class="glyphicon glyphicon-file" <?php echo $style; ?>></i> 
-						<?php else : ?>
-						<?php $style = ''; ?>
-							<i class="glyphicon glyphicon-folder-open" <?php echo $style; ?>></i> 
-						<?php endif; ?>
-							&nbsp;
-							<span <?php echo $style; ?>>
-								<?php echo $folder; ?>
-							</span>
-							<br />
-							<?php echo date('d M Y H:i:s',$staFolder['mtime']); ?>
-					</a>
-				</div>
-				-->
+					<?php 
+					$staFolder = stat($folder);
+					$style = '';
+					$icon = 'glyphicon-folder-open';
+					if(!is_dir($folder)) {
+						$style = 'style="color: red;"';
+						$icon = 'glyphicon-file';
+					}
+					?>
+					
+					<div class="col-md-4 folder" style="height: 100px;">
+						<a class="font-folder" href="<?php echo $folder; ?>" target="_blank" <?php echo $style; ?>>
+							<div class="col-md-3">
+								<i class="glyphicon <?php echo $icon; ?>" style="font-size: 50px;"></i>
+							</div>
+							
+							<div class="col-md-6">
+								<b style="font-size: 14px;">
+									<?php echo $folder; ?>
+								</b>
+								<br />
+								<span style="font-size: 12px;">
+									<?php echo date('d M Y H:i:s',$staFolder['mtime']); ?>
+								</span>
+							</div>
+						</a>
+					</div>
 				
 				<?php endif; ?>
 			<?php endforeach; ?>
 			<div style="clear: both;"> &nbsp; </div>
 			</div>
-
+			
 		</div>
+		
 
 		<script type="text/javascript" src="<?php echo $dir; ?>/bower_components/jquery/dist/jquery.min.js"></script>
 		<script type="text/javascript" src="<?php echo $dir; ?>/bower_components/jquery-backstretch/jquery.backstretch.min.js"></script>
